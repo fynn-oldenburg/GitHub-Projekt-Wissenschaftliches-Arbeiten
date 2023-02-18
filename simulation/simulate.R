76a1bdd0fb3071c1853ee65e4c53cd2ec80c204f
danfleon <daniloffleonel@gmail.com>
2023-02-18 21:06:12 +0100

    try to fix error in R

--- simulation/simulate.R
+++ simulation/simulate.R
@@ -2,21 +2,14 @@
 ## Anzahl Studierende
 n <- 100 
 
-## Studienfach
-studienfach <- sample(c("Statistik", "Data Science", "Mathe", "Informatik"), 
-                      size = n, 
-                      prob = c(.35, .35, .1, .2),
-                      replace = TRUE  
-)
-
 data <- data.frame(
   
   ## Alter 
   "Alter" = floor(rnorm(n, 25, 2)),
   
   ## Studienfach
-  "Studienfach" = studienfach
-   
+  
+  
   ## Interesse an Mathematik
   
   
