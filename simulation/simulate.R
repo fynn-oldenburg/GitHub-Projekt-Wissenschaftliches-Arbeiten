77ca113ab61c9a99d5355aa7c08b7a9a8789f970
Jonas Molsbeck <65602190+jonasmolsbeck@users.noreply.github.com>
2023-02-18 19:45:39 +0100

    Update simulate.R
    Fehler behoben. Vorherige Version.

---
+++ simulation/simulate.R
@@ -0,0 +1,22 @@
+
+## Anzahl Studierende
+n <- 100 
+
+data <- data.frame(
+  
+  ## Alter 
+  "Alter" = floor(rnorm(n, 25, 2)),
+  
+  ## Studienfach
+  
+  
+  ## Interesse an Mathematik
+  
+  
+  ## Interesse an Programmieren
+  
+  
+  ## Mathe-LK (ja/nein)
+  
+  
+)
