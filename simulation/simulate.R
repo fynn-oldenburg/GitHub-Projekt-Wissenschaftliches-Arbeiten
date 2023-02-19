f8106990ea079d321b2abc46712f3c3d8ad8af01
jonasmolsbeck <j.molsbeck@gmx.de>
2023-02-19 13:57:35 +0100

    documentation for interesse-function

--- simulation/simulate.R
+++ simulation/simulate.R
@@ -1,7 +1,8 @@
+library(docstring)
 
 ## Parameter
-n <- 100     ## Anzahl Studierende
-seed <- 123  ## Reproduzierbarkeit
+n <- 100    ## Anzahl Studierende
+seed <- 17  ## Reproduzierbarkeit
 
 
 ## Studienfach
@@ -13,8 +14,16 @@ studienfach <- sample(c("Statistik", "Data Science", "Mathe", "Informatik"),
                       )
 
 
-## Simualtion des Interesses in Abhaengigkeit vom Studienfach 
+## Simulation des Interesses in Abhaengigkeit vom Studienfach 
 interesse <- function(pobStat, probDS, probMath, probInfo, seed) {
+  #' Simulation des Interesses der Studierenden in
+  #' Abhaengigkeit vom Studienfach (1 = wenig, 7 = hohes Interesse)
+  #' 
+  #' @param probStat Vector der Laenge 7. Wahrscheinlichkeits-Gewichte fuer Statistik 
+  #' @param probDS Vector der Laenge 7. Wahrscheinlichkeits-Gewichte fuer Data Science
+  #' @param probMath Vector der Laenge 7. Wahrscheinlichkeits-Gewichte fuer Mathe
+  #' @param probInfo Vector der Laenge 7. Wahrscheinlichkeits-Gewichte fuer Informatik
+  #' @param seed Integer. Reproduzierbarkeit der samples
   set.seed(seed)
   interesse <- sapply(studienfach, function(x){
     if (x == "Statistik") {
@@ -30,6 +39,7 @@ interesse <- function(pobStat, probDS, probMath, probInfo, seed) {
   attributes(interesse) <- NULL
   return(interesse)
 }
+# ?interesse
 
 
 ## Interesse an Mathematik
@@ -37,17 +47,19 @@ mathe.interesse <- interesse(pobStat = c(0, .05, .075, .1, .175, .35, .25),
                              probDS = c(.05, .05, .1, .2, .3, .2, .1), 
                              probMath = c(0, 0, 0, 0, .2, .3, .5), 
                              probInfo = c(.05, .05, .2, .2, .3, .1, .1),
-                             seed = seed)
+                             seed = seed
+                             )
 
 
 ## Interesse an Programmieren
 prog.interesse <- interesse(pobStat = c(0.02, 0.05, 0.15, 0.25, 0.35, 0.15, 0.03), 
-                             probDS = c(0.02, 0.05, 0.15, 0.25, 0.35, 0.15, 0.03), 
-                             probMath = c(0.05, 0.1, 0.2, 0.3, 0.25, 0.08, 0.02), 
-                             probInfo = c(0.05, 0.1, 0.2, 0.3, 0.25, 0.08, 0.02),
-                             seed = seed)
+                            probDS = c(0.02, 0.05, 0.15, 0.25, 0.35, 0.15, 0.03), 
+                            probMath = c(0.05, 0.1, 0.2, 0.3, 0.25, 0.08, 0.02), 
+                            probInfo = c(0.05, 0.1, 0.2, 0.3, 0.25, 0.08, 0.02),
+                            seed = seed
+                            )
 
-
+set.seed(seed)
 data <- data.frame(
   
   ## Alter 
