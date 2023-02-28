ac98bbc9c824087a201b3d4c4d45817f499e22b0
PhilippSL <philipp.schulteloh@gmail.com>
2023-02-28 22:09:17 +0100

    Added a Comment

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -72,9 +72,13 @@ data.vis %>%
 
 stats_metric(data.frame(data$Alter, data$Mathe, data$Programmieren))
 
+
+
 ## Kategoriale Variablen
 
 stats_categorical(data)
+
+
 
 ## bivariate Statistiken für zwei kategoriale Variablen
 
@@ -83,11 +87,21 @@ bivariate_stats_categorical(data, data$Studienfach, data$Programmieren)
 bivariate_stats_categorical(data, data$Studienfach, data$MatheLK)
 bivariate_stats_categorical(data, data$Mathe, data$Programmieren)
 bivariate_stats_categorical(data, data$Mathe, data$MatheLK)
+
+# Ein Cramer's V von über 0.7 spricht für einen starken Zusammenhang 
+# von dem Interesse an Mathe und der Wahl für oder gegen den Mathe-Leistungskurs
+
+
 bivariate_stats_categorical(data, data$Programmieren, data$MatheLK)
+
+
+
 
 ## bivariate Statistiken für eine metrische und eine kategoriale Variable
 
 stats_bivariate_metric_dichotom(data$Alter, data$MatheLK)
+
+
 
 ## Kategorisierung ordinaler Variablen
 
