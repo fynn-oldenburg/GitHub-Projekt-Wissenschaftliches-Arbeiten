05e6db0dba8a3d9f94d49a35bc13ea5496c5d528
danfleon <daniloffleonel@gmail.com>
2023-02-28 14:08:37 +0100

    Documentation of stats_metric.inner

--- functions/helper_functions.R
+++ functions/helper_functions.R
@@ -1,4 +1,19 @@
+library(docstring)
+
 stats_metric.inner <- function(x) {
+  #' stats_metric.inner - berechnet deskriptive Statistiken für metrische Variablen
+  #'
+  #' Die Funktion berechnet die Anzahl, den Mittelwert, die Standardabweichung, das Minimum und das Maximum
+  #' für eine gegebene metrische Variable.
+  #'
+  #' @param x Ein numerischer Vektor der metrischen Variablen.
+  #'
+  #' @return Ein Datenrahmen mit den berechneten Statistiken für die gegebene metrische Variable.
+  #'
+  #' @examples
+  #' stats_metric.inner(c(1, 2, 3, 4, 5))
+  #'
+  #' @export  
   result <- data.frame("Anzahl" = length(x),
                        "Mittelwert" = mean(x),
                        "Standardabweichung" = sd(x),
